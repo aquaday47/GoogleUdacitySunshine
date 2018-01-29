09061abda03bfc8243be002ce9e28770930c0ac4
AquaticNewKeyHack <aaronquaday@gmail.com>
2018-01-29 10:43:45 -0800

    finished basic polish ex 2-3

--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -18,9 +18,13 @@ package com.example.android.sunshine;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.InflateException;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.android.sunshine.data.SunshinePreferences;
@@ -32,11 +36,12 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mWeatherTextView;
+    private static final String TAG = MainActivity.class.getSimpleName();
 
-    // TODO (6) Add a TextView variable for the error message display
-
-    // TODO (16) Add a ProgressBar variable to show and hide the progress bar
-
+    // dONE (6) Add a TextView variable for the error message display
+    private TextView mErrorTV;
+    //DONE (16) Add a ProgressBar variable to show and hide the progress bar
+    private ProgressBar mFetchPB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,10 +53,10 @@ public class MainActivity extends AppCompatActivity {
          */
         mWeatherTextView = (TextView) findViewById(R.id.tv_weather_data);
 
-        // TODO (7) Find the TextView for the error message using findViewById
-
-        // TODO (17) Find the ProgressBar using findViewById
-
+        // DONE (7) Find the TextView for the error message using findViewById
+        mErrorTV = (TextView) findViewById(R.id.tv_error);
+        // DONE (17) Find the ProgressBar using findViewById
+        mFetchPB = (ProgressBar) findViewById(R.id.pb_loading);
         /* Once all of our views are setup, we can load the weather data. */
         loadWeatherData();
     }
@@ -63,16 +68,31 @@ public class MainActivity extends AppCompatActivity {
     private void loadWeatherData() {
         // TODO (20) Call showWeatherDataView before executing the AsyncTask
         String location = SunshinePreferences.getPreferredWeatherLocation(this);
+        showWeatherDataView();
         new FetchWeatherTask().execute(location);
     }
 
     // TODO (8) Create a method called showWeatherDataView that will hide the error message and show the weather data
+    private void showWeatherDataView(){
+        mErrorTV.setVisibility(View.INVISIBLE);
+        mWeatherTextView.setVisibility(View.VISIBLE);
+    }
 
     // TODO (9) Create a method called showErrorMessage that will hide the weather data and show the error message
+    private void  showErrorMessage(){
+        mWeatherTextView.setVisibility(View.INVISIBLE);
+        mErrorTV.setVisibility(View.VISIBLE);
+    }
 
     public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
 
         // TODO (18) Within your AsyncTask, override the method onPreExecute and show the loading indicator
+
+
+        @Override
+        protected void onPreExecute() {
+            mFetchPB.setVisibility(View.VISIBLE);
+        }
 
         @Override
         protected String[] doInBackground(String... params) {
@@ -103,7 +123,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String[] weatherData) {
             // TODO (19) As soon as the data is finished loading, hide the loading indicator
-
+            mFetchPB.setVisibility(View.INVISIBLE);
             if (weatherData != null) {
                 // TODO (11) If the weather data was not null, make sure the data view is visible
                 /*
@@ -111,35 +131,52 @@ public class MainActivity extends AppCompatActivity {
                  * the "\n\n\n" after the String is to give visual separation between each String in the
                  * TextView. Later, we'll learn about a better way to display lists of data.
                  */
+                showWeatherDataView();
                 for (String weatherString : weatherData) {
                     mWeatherTextView.append((weatherString) + "\n\n\n");
                 }
             }
             // TODO (10) If the weather data was null, show the error message
+            else {
+                showErrorMessage();
+            }
+
 
         }
     }
 
+
     @Override
+    //wanted
     public boolean onCreateOptionsMenu(Menu menu) {
-        /* Use AppCompatActivity's method getMenuInflater to get a handle on the menu inflater */
-        MenuInflater inflater = getMenuInflater();
-        /* Use the inflater's inflate method to inflate our menu layout to this menu */
-        inflater.inflate(R.menu.forecast, menu);
-        /* Return true so that the menu is displayed in the Toolbar */
-        return true;
+        boolean succ = true;
+        try {
+            getMenuInflater().inflate(R.menu.forecast, menu);
+            Log.v(TAG, "InflateOccurring");
+
+        }
+        catch (InflateException e){
+            e.printStackTrace();
+            succ = false;
+            Log.v(TAG, "Inflate Exception " + e.toString());
+        }
+        return succ;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
 
+        int id = item.getItemId();
         if (id == R.id.action_refresh) {
+            //sat here researching this for 10 min to find out why setting Text to empty string wasn't working!!!
+            //now it's working!!! -- if I wanted to flash a little info here I could (like res string)
+            //mWeatherTextView.setText(R.string.action_refresh);
+
             mWeatherTextView.setText("");
             loadWeatherData();
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
