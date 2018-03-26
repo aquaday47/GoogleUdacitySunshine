161922c20863cbf18fc89826bc29b83f6d44963d
AquaticNewKeyHack <aaronquaday@gmail.com>
2018-03-25 22:00:22 -0700

    got her syncing properly *added sInit boolean to govern WeatherSync

--- app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
+++ app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
@@ -17,18 +17,54 @@ package com.example.android.sunshine.sync;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
+import android.webkit.WebView;
+
+import com.example.android.sunshine.data.WeatherContract;
 
 
 public class SunshineSyncUtils {
-
+    private static boolean sInit;
 //  TODO (1) Declare a private static boolean field called sInitialized
 
     //  TODO (2) Create a synchronized public static void method called initialize
-    //  TODO (3) Only execute this method body if sInitialized is false
-    //  TODO (4) If the method body is executed, set sInitialized to true
-    //  TODO (5) Check to see if our weather ContentProvider is empty
-        //  TODO (6) If it is empty or we have a null Cursor, sync the weather now!
+    synchronized public static void initialize(@NonNull final Context context) {
+        //  TODO (3) Only execute this method body if sInitialized is false
+        if (sInit) return;
+
+        sInit = true;
+        //  TODO (4) If the method body is executed, set sInitialized to true
+
+        new AsyncTask<Void, Void, Void>() {
+            //  TODO (5) Check to see if our weather ContentProvider is empty
+
+            //  TODO (6) If it is empty or we have a null Cursor, sync the weather now!
+            @Override
+            protected Void doInBackground(Void... voids) {
+
+                Uri weatherUri = WeatherContract.BASE_CONTENT_URI;
+                String[] projCols = {WeatherContract.WeatherEntry._ID};
+                String selectionStatement = WeatherContract.WeatherEntry.getSqlSelectForTodayOnwards();
+
+                Cursor cursor = context.getContentResolver().query(
+                        weatherUri,
+                        projCols,
+                        selectionStatement,
+                        null
+                        , null);
+                if (null == cursor || cursor.getCount() == 0) {
+                    startImmediateSync(context);
+                }
+                cursor.close();
+                return null;
+            }
+
+
+        }.execute();
+    }
 
     /**
      * Helper method to perform a sync immediately using an IntentService for asynchronous
